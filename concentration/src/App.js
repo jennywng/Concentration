913474aa206c298c3cbf8607fd6be6f9938becfb
Jenny <jrw3mx@virginia.edu>
2019-10-09 09:58:36 -0400

    after interview

--- concentration/src/App.js
+++ concentration/src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import GameBoard from "./GameBoard";
+import {Select, MenuItem} from "@material-ui/core";
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            totalPairs: 0,
+        }
+    }
+
+    handleSelect = (e) => {
+        this.setState({totalPairs: e.target.value});
+    };
+
     render() {
         return (
             <div className="App" style={{margin: '10%'}}>
                 <h3>Concentration Game</h3>
-
-                <GameBoard />
+                <Select
+                    value={this.state.totalPairs}
+                    onChange={(e) => this.handleSelect(e)}
+                >
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                </Select>
+                <GameBoard
+                    totalPairs={this.state.totalPairs}
+                />
 
             </div>
         );
--- concentration/src/GameBoard.js
+++ concentration/src/GameBoard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 import MemoryCard from "./MemoryCard";
 
 import './App.css';
@@ -9,13 +9,15 @@ class GameBoard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cards: [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+            cards: [],
             visible: [],
             // to keep track of current turn
             card1: null,
             card2: null,
             cardIdx1: null,
             cardIdx2: null,
+            canClick: true,
+            turns: 0,
         }
     }
 
@@ -34,61 +36,108 @@ class GameBoard extends Component {
         return arr;
     };
 
-    delay = ms => new Promise(res => setTimeout(res, ms));
+    delay = ms => new Promise(res => {
+        // console.log("start", this.state.canClick);
+        // this.setState({canClick: false});
+        // console.log("canClick", this.state.canClick);
+        setTimeout(res, ms);
+        // this.setState({canClick: true});
+        // console.log("end", this.state.canClick);
+    });
 
     setVisible = async (item, idx) => {
-        let cardVal = this.state.cards[idx];
-        console.log("click");
-        if ((this.state.visible.length + 1) % 2 !== 0 || this.state.visible.length === 0) {
-            /** if the middle of a turn **/
-            console.log("setting vis");
-            let copyVisible = this.state.visible.slice();
-            copyVisible.push(idx);
-            this.setState({
-                visible: copyVisible,
-                card1: item,
-                cardIdx1: idx
-            });
-            // this.setState(state => ({
-            //     visible: state.visible.includes(idx) ? state.visible.filter(i => i !== idx) : [...state.visible, idx]
-            // }));
-        } else {
-            /** at the end of a turn **/
-            let copyVisible = this.state.visible.slice();
-            copyVisible.push(idx);
-
-            await this.setState({
-                card2: item,
-                cardIdx2: idx,
-                visible: copyVisible
-            });
-            if (this.state.card1 !== this.state.card2) {
-
-                // wait 3 seconds
-                await this.delay(3000);
-
-                copyVisible.splice(this.state.visible.indexOf(this.state.cardIdx2), 1);
-                copyVisible.splice(this.state.visible.indexOf(this.state.cardIdx1), 1);
+        // let cardVal = this.state.cards[idx];
+        console.log("click", this.state.canClick);
+        if (this.state.canClick) {
+            if ((this.state.visible.length) % 2 === 0) {
+                /** if the middle of a turn **/
+                console.log("setting vis");
+                let copyVisible = this.state.visible.slice();
+                copyVisible.push(idx);
                 this.setState({
-                    visible: copyVisible
+                    visible: copyVisible,
+                    card1: item,
+                    cardIdx1: idx
                 });
-                console.log("mismatch", this.state.visible);
-                console.log("hello");
+                // this.setState(state => ({
+                //     visible: state.visible.includes(idx) ? state.visible.filter(i => i !== idx) : [...state.visible, idx]
+                // }));
+            } else {
+                /** at the end of a turn **/
+                let copyVisible = this.state.visible.slice();
+                copyVisible.push(idx);
+
+                await this.setState({
+                    card2: item,
+                    cardIdx2: idx,
+                    visible: copyVisible,
+                    turns: this.state.turns + 1
+                });
+                if (this.state.card1 !== this.state.card2) {
+
+                    // wait 3 seconds
+                    this.setState({canClick: false});
+                    console.log("canClick", this.state.canClick);
+                    await this.delay(3000);
+                    this.setState({canClick: true});
+                    console.log("end delay", this.state.canClick);
+
+                    copyVisible.splice(this.state.visible.indexOf(this.state.cardIdx2), 1);
+                    copyVisible.splice(this.state.visible.indexOf(this.state.cardIdx1), 1);
+                    this.setState({
+                        visible: copyVisible
+                    });
+                    // console.log("mismatch", this.state.visible);
+                    // console.log("hello");
+                }
             }
         }
     };
 
-    checkValid = (item) => {
-        // loop through the visible array
-        let i1 = this.state.cards.indexOf(item);
-        let i2 = this.state.cards.lastIndexOf(item);
-        return this.state.visible.includes(i1) && this.state.visible.includes(i2);
-    };
-
-    componentDidMount() {
+    reset = () => {
         let copyCards = this.state.cards.slice();
         this.shuffle(copyCards);
-        this.setState({cards: copyCards});
+        this.setState({
+            cards: copyCards,
+            visible: [],
+            card1: null,
+            card2: null,
+            cardIdx1: null,
+            cardIdx2: null,
+            canClick: true,
+            turns: 0,
+        });
+    };
+
+    // componentDidMount() {
+    //     let cards = [];
+    //     for (let i = 0; i < this.props.totalPairs; i++) {
+    //         cards.push(i);
+    //         cards.push(i);
+    //     }
+    //     this.shuffle(cards);
+    //     this.setState({cards: cards});
+    // }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.totalPairs !== prevProps.totalPairs) {
+            let cards = [];
+            for (let i = 0; i < this.props.totalPairs; i++) {
+                cards.push(i);
+                cards.push(i);
+            }
+            this.shuffle(cards);
+            this.setState({
+                cards: cards,
+                visible: [],
+                card1: null,
+                card2: null,
+                cardIdx1: null,
+                cardIdx2: null,
+                canClick: true,
+                turns: 0,
+            });
+        }
     }
 
     render() {
@@ -110,6 +159,13 @@ class GameBoard extends Component {
                         )
                     })}
                 </Grid>
+                <div style={{margin: '5%'}}>
+                    Turns: {this.state.turns}
+                </div>
+
+                {this.state.cards.length > 0 ? (
+                    <Button onClick={this.reset}>New Game</Button>
+                ) : null}
             </div>
         );
     }
